f3435ef0ac6493e0fe67431fdb97bb7e6e58a2f4
varfik <arinavarf@yandex.ru>
2026-02-21 18:51:03 +0300

    update Link

---
+++ src/test/java/pages/elements/Link.java
@@ -0,0 +1,31 @@
+package pages.elements;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+/**
+ * Ссылка
+ */
+public class Link extends BaseElement {
+
+    /**
+     * Конструктор класса
+     */
+    public Link(SelenideElement element) {
+        super(element);
+    }
+
+    /**
+     * Открыть ссылку
+     */
+    public void open() {
+        baseElement.shouldBe(Condition.visible).click();
+    }
+
+    /**
+     * Получить ссылку
+     */
+    public String getHref() {
+        return baseElement.getAttribute("href");
+    }
+}
